d8ea76e78805aaf92d49ffc79c0d40f623a3b979
Orlando Carril Ramirez <omcr1905@gmail.com>
2016-01-16 07:49:20 -0500

    Clase Nueva
    Se agrego una clase para nueva de subir nuevo archivo

---
+++ WebBS/ByS.Presupuesto.Entities/Entities/ClasePrueba.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByS.Presupuesto.Entities
+{
+    class ClasePrueba
+    {
+        /*Esta clase solo se ha subido para reflejar 
+         * preubas y paarq que suba al gitHub como 
+         * archivo nuevo dentro deol proyecto de boticas y salud
+         * mas adelande tiene que ser eliminado o se se usara como 
+         * otra clase */
+    }
+}
