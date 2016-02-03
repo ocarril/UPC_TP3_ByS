a90de813aabaf750fa50bd8362a591ad44fee469
asalinasr <alexander.salinas.romero@gmail.com>
2016-02-02 20:49:52 -0500

    Actualizacion

---
+++ WebBS/ByS.Trazabilidad.Entities/OrdenRetiroEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByS.Trazabilidad.Entities.Entities
+{
+    public class OrdenRetiroEntity
+    {       
+        public string codigoProducto { get; set; }
+        public DateTime? fecha { get; set; }
+        public string autorizado { get; set; }
+        public string motivo { get; set; }
+        public string riesgo { get; set; }
+    }
+}
---
+++ WebBS/Bys.Trazabilidad.Logic/OrdenRetiroLogic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByS.Tools;
+using ByS.Trazabilidad.Data;
+using ByS.Trazabilidad.Entities;
+using ByS.Trazabilidad.Entities.DTO;
+using ByS.Trazabilidad.Entities.Entities;
+
+namespace Bys.Trazabilidad.Logic
+{
+    public class OrdenRetiroLogic
+    {
+        private OrdenRetiroData oData = null;
+        public bool RetirarProducto(OrdenRetiroEntity orden) 
+        {
+            try {
+                oData = new OrdenRetiroData();
+                return oData.RetirarArticulo(orden);                
+            }
+            catch (Exception ex) {
+                throw ex;
+            }            
+        }
+    }
+}
